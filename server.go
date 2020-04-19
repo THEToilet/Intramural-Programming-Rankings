c9025d44f1138d3ee7699439a3e9b1f51a93bf19
THEToilet <bp18015@shibaura-it.ac.jp>
2020-04-20 01:24:51 +0900

    :tada: First Commit

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	values := url.Values{}
+	values.Add("user", "Toilet")
+	resp, err := http.Get("https://kenkoooo.com/atcoder/atcoder-api/v2/user_info" + "?" + values.Encode())
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	execute(resp)
+}
+
+func execute(response *http.Response) {
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+}
