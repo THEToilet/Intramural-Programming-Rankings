23d06a2b05fe55921d5d6e2cfe6f28d1818cc693
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-04 18:42:44 +0900

    :art: Clean user_load.go

--- user_load.go
+++ user_load.go
@@ -7,19 +7,14 @@ import (
 )
 
 func loadFile(filePath string) []string {
-	// ファイルを開く
 	f, err := os.Open(filePath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "File %s could not read: %v\n", filePath, err)
 		os.Exit(1)
 	}
 
-	// 関数return時に閉じる
 	defer f.Close()
 
-	// Scannerで読み込む
-
-	// lines := []string{}
 	lines := make([]string, 0, 100) // ある程度行数が事前に見積もれるようであれば、makeで初期capacityを指定して予めメモリを確保しておくことが望ましい
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
